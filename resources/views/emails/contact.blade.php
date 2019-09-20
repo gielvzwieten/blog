ee5cfa3218982df7d6b8a92b52bcccefbb914cba
Giel van Zwieten <gielvzwieten@gmail.com>
2019-09-20 19:48:05 +0200

    new

--- resources/views/emails/contact.blade.php
+++
@@ -1,5 +0,0 @@
-<h3>You have a new Contact via the contact form</h3>
-<div>
-    {{$bodyMessage}}
-</div>
-
--- resources/views/partials/_footer.blade.php
+++
@@ -1,4 +0,0 @@
-<hr>
-<footer class="container row justify-content-center mt-5">
-   <p>Copywrite &copy; {{date('Y')}} Giel van Zwieten</p>
-</footer>
